b52312b711e41016615dd67e3772767081e490e8
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2018-10-15 15:40:07 +0200

    Added sections 4 and 5 of chapter 1

---
+++ ch01/05 - GettingAround.ps1
@@ -0,0 +1,27 @@
+# All cmdlets on a system
+Get-Command
+
+# Most important parameter Syntax to display the cmdlet syntax
+Get-Command New-Item -Syntax
+
+# List all cmdlets with a specific verb from one or more modules
+Get-Command -Verb Get -Module Microsoft.PowerShell.Utility
+
+# List all external applications and libraries
+Get-Command -CommandType Application
+
+# Very interesting as well - searching for imported cmdlets by their parameters
+# ComputerName,CimSession and PSSession indicate remoting capabilities
+Get-Command -ParameterName ComputerName,CimSession,PSSession
+
+# Performing a wildcard search through the cmdlet gallery
+Get-Command *Process,*Item
+
+# New-Alias will be used to create aliases. Aliases will overwrite existing
+# cmdlets without hesitation. Have a look at Get-Help about_Command_precedence for more information
+New-Alias -Name Start-Process -Value hostname
+Get-Command Start-Process
+
+# But Get-Command will help yet again and display all instances of a command
+# listing everything with the same name
+Get-Command Start-Process -All
